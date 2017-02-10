86deaa66ff109a182c93ae55bcd68c93deefc1c4
Michael Liu <lium039@mgail.com>
2017-02-10 14:29:50 -0800

    done

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,13 @@
 "use strict";
 module.exports = {
-
+    production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
+    
+    },
     development: {
         client: 'postgresql',
-        connection: "postgres://localhost/blog",
+        connection: process.env.DATABASE_URL || "postgres://localhost/blog",
         pool: {
             min: 1,
             max: 1
@@ -11,7 +15,7 @@ module.exports = {
     },
     test: {
         client: 'postgresql',
-        connection: "postgres://localhost/test-blog",
+        connection: process.env.DATABASE_URL || "postgres://localhost/test-blog",
         pool: {
             min: 1,
             max: 1
